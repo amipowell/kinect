d6db8e0dda50016d2b2aa6706d9c6a484101efa9
Gabriel Bussieres <gab.bussieres@gmail.com>
2015-06-10 16:57:47 -0400

    Added ability to switch backgrounds
    On click for now. Import a picture (texture) as a new asset. Create a
    material and add the texture to it. In the Inspector for Background, go
    to the Change Background script section and drag the material onto
    Textures.

---
+++ KinectUnityPhotobooth/Assets/Scripts/ChangeBackground.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeBackground : MonoBehaviour {
+
+	public Material[] textures;
+	public Renderer rend;
+
+	private int index = 0;
+	
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	
+	void Update() {
+		if (textures.Length == 0)
+			return;
+		if (Input.GetButtonDown ("Fire1")) {
+			index++;
+			index = index % textures.Length;
+			renderer.material = textures[index];
+		}
+	}
+}
