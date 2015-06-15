7509f31dd917a5b0d7f529c1166d8789228ba54e
amipowell <theamipowell@gmail.com>
2015-06-15 19:01:52 -0400

    Added another background option.

---
+++ KinectUnityPhotobooth/Assets/UnityScripts/MammothScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MammothScript : MonoBehaviour
+{
+
+    public Material material;
+    public Renderer rend;
+
+    public void ChangeBackground()
+    {
+        rend.material = material;
+    }
+}
