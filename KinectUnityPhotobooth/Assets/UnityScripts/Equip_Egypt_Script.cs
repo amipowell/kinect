ebe2e422ef1132879ff5d6e944d3b1ee5dbc39e5
Gabriel Bussieres <gab.bussieres@gmail.com>
2015-07-05 12:12:35 -0400

    Can change outfit

---
+++ KinectUnityPhotobooth/Assets/UnityScripts/Equip_Egypt_Script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Equip_Egypt_Script : MonoBehaviour {
+
+	public GameObject[] Armour;
+	public GameObject[] Egypt;
+	
+	void Start () {
+	}
+
+	public void EquipEgypt () {
+		Armour = GameObject.FindGameObjectsWithTag ("Armour");
+
+		for (int i = 0; i < Armour.Length; i++) {
+			Armour[i].SetActive(false);
+		}
+		
+		for (int i = 0; i < Egypt.Length; i++) {
+			Egypt[i].SetActive(true);
+		}
+	}
+}
