625ecea40e689a9cb81db54d2404904e381573f6
Gabriel Bussieres <gab.bussieres@gmail.com>
2015-07-05 20:35:45 -0400

    Can take screenshots

---
+++ KinectUnityPhotobooth/Assets/UnityScripts/Screenshot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshot : MonoBehaviour {
+
+	public GameObject[] Panels;
+
+	public void OnMouseDown() {
+
+		for (int i = 0; i < Panels.Length; i++) {
+			Panels[i].SetActive(false);
+		}
+
+		Invoke ("TakePicture", 2);
+	}
+
+	void TakePicture() {
+		Application.CaptureScreenshot("C:/Users/Gabriel/Pictures/Kinect/Screenshot.png");
+		Invoke ("SetPanelsActive", 2);
+	}
+
+	void SetPanelsActive() {
+		for (int i = 0; i < Panels.Length; i++) {
+			Panels[i].SetActive(true);
+		}
+	}
+}
