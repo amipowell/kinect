c04c80b0d8acb903d3b67a9f32c25de279ab9260
Gabriel Bussieres <gab.bussieres@gmail.com>
2015-07-06 00:55:39 -0400

    Picture displayed on menu

--- KinectUnityPhotobooth/Assets/UnityScripts/Screenshot.cs
+++ KinectUnityPhotobooth/Assets/UnityScripts/Screenshot.cs
@@ -1,27 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.IO;
 
 public class Screenshot : MonoBehaviour {
 
 	public GameObject[] Panels;
+	public GameObject TweetMenu;
+	public Sprite PictureSprite;
+	public Image Picture;
+	public Texture2D tex;
+	public byte[] fileData;
 
 	public void OnMouseDown() {
-
 		for (int i = 0; i < Panels.Length; i++) {
 			Panels[i].SetActive(false);
 		}
-
-		Invoke ("TakePicture", 2);
+		Invoke ("TakePicture", 3);
 	}
 
 	void TakePicture() {
 		Application.CaptureScreenshot("C:/Users/Gabriel/Pictures/Kinect/Screenshot.png");
-		Invoke ("SetPanelsActive", 2);
+		Invoke ("Menu", 1);
 	}
 
-	void SetPanelsActive() {
+	void Menu() {
+		if (File.Exists("C:/Users/Gabriel/Pictures/Kinect/Screenshot.png")) {
+			fileData = File.ReadAllBytes("C:/Users/Gabriel/Pictures/Kinect/Screenshot.png");
+			tex = new Texture2D(2, 2);
+			tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+		}
+		PictureSprite = Sprite.Create (tex, new Rect(0, 0, 1352, 675), new Vector2(0, 0));
+		Picture.sprite = PictureSprite;
+		TweetMenu.SetActive (true);
+	}
+
+	public void Tweet() {
+
+	}
+
+	public void Retake() {
+		TweetMenu.SetActive (false);
 		for (int i = 0; i < Panels.Length; i++) {
-			Panels[i].SetActive(true);
+			Panels[i].SetActive (true);
 		}
 	}
 }
