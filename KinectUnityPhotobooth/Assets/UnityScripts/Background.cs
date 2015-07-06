1caa0164416ead87f088024875058f458a5d5367
Gabriel Bussieres <gab.bussieres@gmail.com>
2015-07-06 14:02:33 -0400

    Improved scripts

---
+++ KinectUnityPhotobooth/Assets/UnityScripts/Background.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	public Material[] materials;
+	public Renderer background;
+	
+	public void DinoBackground () {
+		background.material = materials[0];
+	}
+
+	public void EgyptBackground () {
+		background.material = materials[1];
+	}
+
+	public void AquaBackground () {
+		background.material = materials[2];
+	}
+
+	public void MammothBackground () {
+		background.material = materials[3];
+	}
+}
---
+++ KinectUnityPhotobooth/Assets/UnityScripts/Equip.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Equip : MonoBehaviour {
+
+	public GameObject[] Armour, Egypt;
+
+	private GameObject[][] EquipArray = new GameObject[2][];
+	private GameObject[] ActiveEquip, PrevEquip;
+
+	void Start () {
+		EquipArray [0] = Armour;
+		EquipArray [1] = Egypt;
+		PrevEquip = Armour;
+	}
+
+	public void EquipArmour () {
+		PrevEquip = ActiveEquip;
+		ActiveEquip = Armour;
+		EquipAvatar ();
+	}
+
+	public void EquipEgypt () {
+		PrevEquip = ActiveEquip;
+		ActiveEquip = Egypt;
+		EquipAvatar ();
+	}
+
+	void EquipAvatar () {
+		for (int i = 0; i < EquipArray.Length; i++) {
+			if (EquipArray[i] == ActiveEquip){
+				EquipLoop (i, true);
+			} else if (EquipArray[i] == PrevEquip){
+				EquipLoop (i, false);
+			}
+		}
+	}
+
+	void EquipLoop (int index, bool active) {
+		for (int obj = 0; obj < EquipArray[index].Length; obj++) {
+			EquipArray[index][obj].SetActive (active);
+		}
+	}
+}
